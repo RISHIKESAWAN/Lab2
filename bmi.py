fde09a5e3f9c61f4770bfb07b7064e3f5d60efa9
Rishikesawan <RRISHIKESAWAN28.23@ichat.sp.edu.sg>
2024-10-30 11:51:39 +0800

    Updated

---
+++ bmi.py
@@ -0,0 +1,16 @@
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+ bmi = weight/(height**2)
+ print("Calculated BMI = " + str(bmi))
+ 
+ # Determine weight classification based on BM
+ if bmi < 18.5:
+    classification = "Under Weight"
+ elif 18.5 <= bmi <= 25.0:
+    classification = "Normal Weight"
+ else:
+    classification = "Over Weight"
+ print("Weight Classification = " + classification)
+ 
+calculate_bmi(weight=57, height=1.73)
