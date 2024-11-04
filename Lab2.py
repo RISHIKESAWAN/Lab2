a6cd10c45b47064bcb1dca23e7994ecbc82d163f
Rishikesawan <RRISHIKESAWAN28.23@ichat.sp.edu.sg>
2024-11-04 17:30:54 +0800

    Finds the median in the temp list

--- Lab2.py
+++ Lab2.py
@@ -1,5 +1,5 @@
 def display_main_menu():
-    print("“display_main_menu")
+    print("display_main_menu")
 
 def get_user_input():
     user_input = input("Enter a sequence of numbers separated by commas (e.g., 5,67,32): ")
@@ -18,16 +18,30 @@ def find_min_max(temperatures):
     return [int(min_temp), int(max_temp)]  # Return as a list of integers
 
 def sort_temperature(temperatures):
-    print("sort_temperature")
-    # This will return the sorted list of temperatures later
-    return []
+    sorted_temps = sorted(temperatures)
+    return sorted_temps
 
 def calc_median_temperature(temperatures):
-    print("calc_median_temperature")
-    # This will return the median temperature later
-    return 0.0
+    sorted_temps = sorted(temperatures)  # Sort the temperatures
+    n = len(sorted_temps)  # Find the number of elements
+    # Calculate the median
+    if n % 2 == 1:  # Odd number of elements
+        median = sorted_temps[n // 2]
+    else:  # Even number of elements
+        median = (sorted_temps[n // 2 - 1] + sorted_temps[n // 2]) / 2
+    return median
 
-display_main_menu()
-temperatures = get_user_input() #Get's the User Inputs
-average = calc_average(temperatures)
-print("The Average is "+ str(average))
+def main():
+    display_main_menu()
+    temperatures = get_user_input()  # Get user input as a list of floats
+    average = calc_average(temperatures)
+    print("The Average is " + str(average))
+    min_max = find_min_max(temperatures)
+    print("The minimum temperature is " + str(min_max[0]) + " and the maximum temperature is " + str(min_max[1]))
+    sorted_temps = sort_temperature(temperatures)
+    print("The sorted temperatures are:", sorted_temps)
+    median = calc_median_temperature(temperatures)
+    print("The median temperature is " + str(median))
+    
+if __name__ == "__main__":
+ main()
